d79eff207dbb80b7d09768e20c73736dea64517d
Nikolay Vitkov <lightning00blade@gmail.com>
2021-07-09 21:03:48 +0300

    [Fix] Fonts
    Signed-off-by: Nikolay Vitkov <lightning00blade@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,17 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Oswald&family=Roboto&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/css/fonts.css'],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
