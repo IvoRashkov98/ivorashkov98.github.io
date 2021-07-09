bf362aa4e62f41476e25c3503b633d5c5ce13d62
Nikolay Vitkov <lightning00blade@gmail.com>
2021-07-09 20:46:48 +0300

    [Fix] Nuxt Class Decorator
    Signed-off-by: Nikolay Vitkov <lightning00blade@gmail.com>

--- components/Header.vue
+++ components/Header.vue
@@ -139,7 +139,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component } from 'nuxt-property-decorator';
 
 type Link = {
   anchor: string;
